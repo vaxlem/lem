f20ab21b98808320f5f95d292c2ea87177b1298e
hermonsel <vaxlem5@gmail.com>
2024-09-28 05:13:07 +0200

    pip first commit

--- my_flask_app.py
+++ my_flask_app.py
@@ -1,12 +1,26 @@
-# app.py
+# A simple password check
+allowed_passwords = {5, 10, 3, 8, 4}  # Allowed passwords
 
-from flask import Flask
+def login():
+    try:
+        # Get user input
+        password = int(input("Enter the password: "))
+        
+        # Check if the entered password is in the allowed passwords set
+        if password in allowed_passwords:
+            print("Access granted.")
+            # Function to access the Google Drive document (pseudo code)
+            access_google_drive_document()
+        else:
+            print("Access denied. Incorrect password.")
+    
+    except ValueError:
+        print("Invalid input. Please enter a number.")
 
-app = Flask(__name__)
+def access_google_drive_document():
+    # This function represents accessing a Google Drive document
+    # Actual implementation would involve integrating Google Drive API
+    print("Accessing document from Google Drive...")
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Run the login
+login()
